8b6227e6940fd8265650ebef72807a6080dca2bc
Jiyan <arogithub@gmail.com>
2021-04-01 17:02:59 +0200

    En outprint med HelloGit

---
+++ ScrumProject/src/scrumproject/ScrumProject.java
@@ -0,0 +1,21 @@
+/*
+ *Vårat projekt som heter scrumproject
+ */
+package scrumproject;
+
+/**
+ *
+ * @author 46768 Grupp 7. Jiyan, Saron, Carolina, Fatimeh, Victoria, Samer, Manne, Oday, Ramin, Michael
+ 
+ * 
+ */
+public class ScrumProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello Github och Git.desktop");
+    }
+    
+}
